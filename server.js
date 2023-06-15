af74bdb52b50171fa53700784c5dfdd6aa2be15d
abanob morkos <abanobmorkos@abanobs-air.myfiosgateway.com>
2023-06-14 20:25:51 -0400

    hw2 wk7

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const drinks = require("./models/drinks");
+const drinks = require("./models/drinks.js");
 const app = express()
 const PORT=3333;
 
@@ -8,14 +8,27 @@ const PORT=3333;
 app.get("/" , (req , res) => {
     res.send(`<h1> welcome to the Gitpub App! </h1>`)
 })
+// Setting up your show route
+
+app.get("/drinks/:id", (req, res) => {
+    const id = (req.params.id)
+    const drink = drinks[id];
+    res.render("show.ejs", { drinks : drinks });
+});
+
+app.get("/show.ejs" , (req ,res) => {
+    const id = req.params.id
+    const drink = drinks[id];
+    res.render("show.ejs")
+})
 
 // Database
-
 app.get("/drinks" , (req ,res) => {
-    res.render("./index.ejs")
+    res.render("./index.ejs" , {drinks : drinks})
 })
 
 // LISTEN 
 app.listen(PORT , (req , res) => {
     console.log(`Listeing on ${PORT}`)
 })
+
